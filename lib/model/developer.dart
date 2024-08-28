113c74c666aede5641030e7fcfa9524a31f6a350
dlcode036462 <davisonluis4@gmail.com>
2024-08-28 13:59:16 -0300

    assets

--- lib/model/developer.dart
+++ lib/model/developer.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 class Developer {
   final String name;
   final String group;
+  String? serie;
   final String image;
 
   Developer({
     required this.name,
     required this.group,
+    this.serie,
     required this.image,
   });
 }
--- lib/pages/developersPage.dart
+++ lib/pages/developersPage.dart
@@ -7,14 +7,16 @@ import '../model/developer.dart';
 class DevelopersPage extends StatelessWidget {
   final List<Developer> developers = [
     Developer(name: 'Davison Luis Soares Lopes', group: 'Coordenador', image: 'assets/davison.jpg'),
-    Developer(name: 'Maria', group: 'Professor', image: 'assets/aleh.jpg'),
-    Developer(name: 'Alerhandro De Ol. Gomes', group: 'Aluno', image: 'assets/aleh.jpg'),
-    Developer(name: 'Pedro Henrique De Ol. Porfirio', group: 'Aluno', image: 'assets/pedro.png'),
-    Developer(name: 'Pedro Henrique De Ol. Porfirio', group: 'Aluno', image: 'assets/pedro.png'),
-    Developer(name: 'yara maryrlla f. de oliveira', group: 'Aluno', image: 'assets/pedro.png'),
-    Developer(name: 'Pedro Henrique De Ol. Porfirio', group: 'Aluno', image: 'assets/pedro.png'),
-    Developer(name: 'Pedro Henrique De Ol. Porfirio', group: 'Aluno', image: 'assets/pedro.png'),
-    Developer(name: 'Pedro Henrique De Ol. Porfirio', group: 'Aluno', image: 'assets/pedro.png'),
+    Developer(name: 'Tiago Bezerra', group: 'Professor', image: 'assets/tiago.png'),
+    Developer(name: 'Elisama Cardoso Rosendo', group: 'Aluno', serie: "3° Série", image: 'assets/elisama.jpg'),
+    Developer(name: 'Alerhandro De O. Gomes', group: 'Aluno', serie: "2° Série", image: 'assets/aleh.jpg'),
+    Developer(name: 'José Marcos Silva de Jesus', group: 'Aluno', serie: "2° Série", image: 'assets/marcos.jpg'),
+    Developer(name: 'Matheus Souza de Pontes ', group: 'Aluno', serie: "2° Série", image: 'assets/mateus.jpg'),
+    Developer(name: 'Jonathas Gomes B. da silva', group: 'Aluno', serie: "2° Série", image: 'assets/jonata.jpg'),
+    Developer(name: 'Pedro Henrique De O. Porfirio', group: 'Aluno',  serie: "1° Ano", image: 'assets/pedro.png'),
+    Developer(name: 'yara maryrlla f. de oliveira', group: 'Aluno', serie: "1° Ano", image: 'assets/yara.jpg'),
+    Developer(name: 'Maria Nayara S. do Nascimento', group: 'Aluno', serie: "1° Ano", image: 'assets/maria.jpg'),
+    Developer(name: 'Leticia Silva dos Santos', group: 'Aluno', serie: "1° Ano", image: 'assets/leticia.jpg'),
 
 
   ];
@@ -22,8 +24,15 @@ class DevelopersPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Desenvolvedores'),
+      appBar:AppBar(
+        title: const Text(
+          'Desenvolvedores',
+          style: TextStyle(color: Colors.white),
+        ),
+
+        iconTheme: const IconThemeData(color: Colors.white),
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.red,
       ),
       body: ListView.builder(
         itemCount: developers.length,
